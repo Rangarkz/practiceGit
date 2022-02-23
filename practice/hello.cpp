0ba53fc2daf4b4670495a975e205e95fb93376df
Ranga Reddy <srr66166@gmail.com>
2022-02-23 12:34:01 +0530

    added all files i have

---
+++ practice/hello.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N=1e5;
+void solve(){
+	int n;
+	cin>>n;
+	vector<int> adj[n];
+	for(int i=0;i<n-1;i++)
+}
+int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	solve();
+	return 0;
+}
---
+++ practice/hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class hello{
+	public static void main(String[] args) {
+		Scanner ip=new Scanner(System.in);
+		String str=ip.nextLine();
+		System.out.println(str);
+	}
+}
